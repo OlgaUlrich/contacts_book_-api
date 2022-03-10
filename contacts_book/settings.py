66d5128eae9334a386d98852b1bd61ed8c4daf48
Olga_Ulrich <olga.ulrich.ch@gmail.com>
2022-03-10 18:41:49 +0100

    CORS added

--- contacts_book/settings.py
+++ contacts_book/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',
+    'corsheaders',
     'rest_framework',
     'authentication',
     'contacts'
@@ -78,6 +79,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -146,6 +148,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+#Cors
+
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+]
 
 
 # JWT
